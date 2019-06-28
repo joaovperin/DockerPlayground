f7ed72cfe3a53b52e6bad5afd159c53cdd5f1a86
João Perin <joaovperin@live.com>
2019-06-28 13:40:39 -0300

    Translated docker-build.cmd to shellscript

---
+++ Containers/MavenJdk8_Shared/docker-build.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+CONTAINER_NAME=maven-jdk-8-localcache
+BASE=$(pwd)
+LOCAL_MVN_CACHE=$HOME/.m2
+PARAM_ADD=
+
+CMD_RUN=docker run -v $BASE:/src -v $LOCAL_MVN_CACHE:/root/.m2 $PARAM_ADD $CONTAINER_NAME $@
+echo %CMD_RUN%
+exec $CMD_RUN
