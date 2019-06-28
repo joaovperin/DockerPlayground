1f62a742cc07e4737911b4a865cbe761a45596ce
João Perin <joaovperin@live.com>
2019-06-28 14:05:24 -0300

    Fixed docker-build.sh

--- Containers/MavenJdk8_Shared/docker-build.sh
+++ Containers/MavenJdk8_Shared/docker-build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-CONTAINER_NAME=maven-jdk-8-localcache
+CONTAINER_NAME=maven-jdk8-shared
 BASE=$(pwd)
 LOCAL_MVN_CACHE=$HOME/.m2
 PARAM_ADD=
 
 CMD_RUN=docker run -v $BASE:/src -v $LOCAL_MVN_CACHE:/root/.m2 $PARAM_ADD $CONTAINER_NAME $@
-echo %CMD_RUN%
+echo $CMD_RUN
 exec $CMD_RUN
